615b339b99e2c5a37b0a28e4e7e018c0822e213a
Bang Cao <caothanhbang2002@gmail.com>
2022-08-14 16:24:02 +0700

    prod

--- allowedContent.js
+++ allowedContent.js
@@ -1,30 +1,28 @@
 export const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://ka-f.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://api.mapbox.com/",
+	"https://kit.fontawesome.com/",
+	"https://ka-f.fontawesome.com/",
+	"https://cdnjs.cloudflare.com/",
+	"https://cdn.jsdelivr.net",
 ];
 export const styleSrcUrls = [
-    "https://cdn.jsdelivr.net/",
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://ka-f.fontawesome.com/",
+	"https://cdn.jsdelivr.net/",
+	"https://kit-free.fontawesome.com/",
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.mapbox.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://fonts.googleapis.com/",
+	"https://use.fontawesome.com/",
+	"https://ka-f.fontawesome.com/",
 ];
 export const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://ka-f.fontawesome.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
+	"https://post-it-fullstack.herokuapp.com/",
+	"https://api.mapbox.com/",
+	"https://ka-f.fontawesome.com/",
+	"https://a.tiles.mapbox.com/",
+	"https://b.tiles.mapbox.com/",
+	"https://events.mapbox.com/",
 ];
-export const fontSrcUrls = [
-    "https://ka-f.fontawesome.com/",
-];
-
+export const fontSrcUrls = ["https://ka-f.fontawesome.com/"];
