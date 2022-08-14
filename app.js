ba11bd05958317c95fcf34e4adce220fb46298ac
Bang Cao <caothanhbang2002@gmail.com>
2022-08-14 14:19:24 +0700

    fix bugs

--- app.js
+++ app.js
@@ -123,11 +123,9 @@ app.use(passport.session());
 localStrategy(passport);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+	app.use(express.static(path.resolve(__dirname, "./frontend/build")));
 	app.get("*", (req, res) => {
-		res.sendFile(
-			path.resolve(__dirname, "../frontend/build", "index.html")
-		);
+		res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
 	});
 }
 
